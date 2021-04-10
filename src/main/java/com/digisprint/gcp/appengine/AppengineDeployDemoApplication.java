90d760cf2b5ac08a03a20e8f154c37be1e83c514
baladigisprint <81316901+baladigisprint@users.noreply.github.com>
2021-04-11 01:03:26 +0530

    initial commit

---
+++ src/main/java/com/digisprint/gcp/appengine/AppengineDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.digisprint.gcp.appengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppengineDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppengineDeployDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digisprint/gcp/appengine/AppengineDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digisprint.gcp.appengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppengineDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
